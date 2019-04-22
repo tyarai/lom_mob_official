dba6bfb7542bce9186805cb4bdfff18c511b4c1b
Ranto Andrianavonison <ranto@tyarai.com>
2019-04-22 22:50:19 +0300

    DrwaerMenu part#1 done

--- lemurs_of_madagascar/lib/screens/Introduction.dart
+++ lemurs_of_madagascar/lib/screens/Introduction.dart
@@ -21,6 +21,11 @@ class IntroductionPage extends StatefulWidget {
 }
 
 class _IntroductionPageState extends State<IntroductionPage> {
+
+  var _menuItemFontSize = 18.0;
+  var _iconSize = 24.0;
+
+
   DatabaseHelper _databaseHandler = DatabaseHelper();
   MenuDatabaseHelper _menuDatabaseHandler = MenuDatabaseHelper();
 
@@ -30,6 +35,7 @@ class _IntroductionPageState extends State<IntroductionPage> {
 
   @override
   Widget build(BuildContext context) {
+    
     if (menuList == null) {
       menuList = List<Menu>();
       _updateUI();
@@ -40,6 +46,11 @@ class _IntroductionPageState extends State<IntroductionPage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: _getDrawerMenu(),
+        ),
       ),
       body: Padding(
           padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
@@ -82,4 +93,79 @@ class _IntroductionPageState extends State<IntroductionPage> {
       });
     });
   }
+
+
+  List<Widget> _getDrawerMenu(){
+
+    List<Widget> menuItems = List<Widget>();
+
+    menuItems.add(ListTile(
+      title: Text("Introduction",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/ico_infos.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Sightings",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/binocular.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Primate watching",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/ico_eye.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Origin of lemurs",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/origin.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Extinct lemurs",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/extinct.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("About the authors",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/author.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Species",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/ico_specy.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Families",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/ico_families.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Watching sites",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/ico_map.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Our partners",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/ico_partners.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Settings",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/settings_icon.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("Report illegal activities",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/sign_forbidden.png",width:_iconSize,height:_iconSize),
+    ));
+
+    menuItems.add(ListTile(
+      title: Text("App instructions",style:TextStyle(fontSize: _menuItemFontSize)),
+      leading: Image.asset("assets/images/icons/about.png",width:_iconSize,height:_iconSize),
+    ));
+
+    return menuItems;
+  }
+
+
 }
