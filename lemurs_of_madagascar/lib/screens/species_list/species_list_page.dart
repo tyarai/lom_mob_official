27a049514d10856f8ad8ff886945b93a12623146
Ranto Andrianavonison <ranto@tyarai.com>
2019-04-27 18:35:33 +0300

    Done ListView layout

--- lemurs_of_madagascar/lib/screens/species_list/species_list_page.dart
+++ lemurs_of_madagascar/lib/screens/species_list/species_list_page.dart
@@ -15,19 +15,23 @@ class SpeciesListPage extends StatefulWidget {
   }
 }
 
-class SpeciesListPageState extends State<SpeciesListPage> {
+enum CellType {
+  ListTiles,
+  Column,
+  FittedBox,
+}
 
+class SpeciesListPageState extends State<SpeciesListPage> {
   List<Species> _speciesList = List<Species>();
 
   SpeciesListPageState();
-
 
   @override
   void initState() {
     super.initState();
 
     // Only do this one time if the List is empty
-    if(_speciesList.length == 0) {
+    if (_speciesList.length == 0) {
       Future<List<Species>> futureList = _loadData();
       futureList.then((list) {
         setState(() {
@@ -35,7 +39,6 @@ class SpeciesListPageState extends State<SpeciesListPage> {
         });
       });
     }
-
   }
 
   Future<List<Species>> _loadData() async {
@@ -50,42 +53,51 @@ class SpeciesListPageState extends State<SpeciesListPage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _buildSpeciesGridList(),
-
+      body: _buildSpeciesListView(),
     );
   }
 
-  Widget _loadImage(String fileName, {double width = Constants.listViewImageWidth , double height = Constants.listViewImageWidth}){
-
-    return
-      Image.asset(
-        fileName,
-        width: width,
-        height: height,
-
-      );
+  Widget _loadImage(String fileName,
+      {double width = Constants.listViewImageWidth,
+      double height = Constants.listViewImageWidth}) {
+    return Image.asset(
+      fileName,
+      width: width,
+      height: height,
+    );
   }
 
-  Widget _buildSpeciesGridList() {
-
+  Widget _buildSpeciesListView() {
     return FutureBuilder<List<Species>>(
-
       future: _loadData(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-
-        if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
-
+        if (!snapshot.hasData)
+          return Center(child: CircularProgressIndicator());
 
         return ListView.builder(
-            itemCount: snapshot.data.length ,
+            scrollDirection: Axis.vertical,
+            itemCount: snapshot.data.length,
             itemBuilder: (BuildContext context, int index) {
-
               return _buildCellItem(index);
-
             });
       },
     );
+  }
 
+  Widget _buildSpeciesGridList() {
+    return FutureBuilder<List<Species>>(
+      future: _loadData(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData)
+          return Center(child: CircularProgressIndicator());
+
+        return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _buildCellItem(index);
+            });
+      },
+    );
   }
 
   /*Widget _buildSpeciesGridList() {
@@ -120,29 +132,86 @@ class SpeciesListPageState extends State<SpeciesListPage> {
   }
   */
 
-  Widget _buildCellItem( int index) {
-
+  Widget _buildCellItem(int index, {CellType cellType = CellType.FittedBox}) {
     Species species = _speciesList[index];
 
-    return Padding(
-      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-      child: Card(
-        elevation: 2.5,
-      child: ListTile(
-      leading:Container(
-                      height: Constants.listViewImageHeight,
-                      width: Constants.listViewImageWidth,
-                      child:
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(25.0),
-                          child:_loadImage(species.imageFile))),
-      title:Container(
-                      height: Constants.listViewImageHeight,
-                      width: Constants.listViewImageWidth,
-                      child:Text(species.title)),
-      )),
-     );
+    Widget widget;
 
+    switch (cellType) {
+      case CellType.Column:
+        {
+          break;
+        }
+      case CellType.FittedBox:
+        {
+          widget = Padding(
+              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+              child: Container(
+                  child: Material(
+                elevation: 2.5,
+                borderRadius: BorderRadius.circular(15.0),
+                shadowColor: Colors.blueGrey,
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(5, 5, 10, 5),
+                    child:Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      _buildLemurPhoto(species),
+                      Container(width: 10),
+                      _buildTextInfo(species),
+                    ])
+                ),
+              )));
 
+          break;
+        }
+      case CellType.ListTiles:
+        {
+          widget = Padding(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: Card(
+                elevation: 1.5,
+                child: ListTile(
+                  leading: _buildLemurPhoto(species),
+                  title: _buildTitle(species),
+                  subtitle: _buildSubTitle(species),
+                ),
+              ));
+          break;
+        }
+    }
+
+    return widget;
+  }
+
+  Widget _buildTextInfo(Species species) {
+    return Expanded(
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(species.title, style: TextStyle(fontSize: Constants.titleFontSize)),
+            Container(height: 10),
+            Text(species.malagasy,style: TextStyle(fontSize: Constants.subTitleFontSize)),
+          ]),
+    );
+  }
+
+  Widget _buildLemurPhoto(Species species) {
+    return Container(
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: _loadImage(species.imageFile)));
+  }
+
+  Widget _buildTitle(Species species) {
+    return Expanded(
+      child: Text(species.title, style: TextStyle(fontSize: 18.0)),
+    );
+  }
+
+  Widget _buildSubTitle(Species species) {
+    return Expanded(
+      child: Text(species.malagasy),
+    );
   }
 }
--- lemurs_of_madagascar/lib/utils/constants.dart
+++ lemurs_of_madagascar/lib/utils/constants.dart
@@ -1,8 +1,11 @@
 
 class Constants {
 
-  static const double listViewImageWidth = 100.0;
-  static const double listViewImageHeight = 100.0;
+  static const double listViewImageWidth = 120.0;
+  static const double listViewImageHeight = 120.0;
+  static const double titleFontSize = 20.0;
+  static const double subTitleFontSize = 16.0;
+
 
 
 }
