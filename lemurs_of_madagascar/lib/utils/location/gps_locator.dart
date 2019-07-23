5ecd544e9d4d680994f2a055466c0622a5a1dbce
NYR FLY <nyrfly@iMac-EcoProd.local>
2019-07-23 18:17:01 +0300

    Fixed GPS Location on Android

--- lemurs_of_madagascar/lib/utils/location/gps_locator.dart
+++ lemurs_of_madagascar/lib/utils/location/gps_locator.dart
@@ -1,4 +1,5 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class GPSLocator{
 
@@ -8,31 +9,56 @@ class GPSLocator{
     return await Geolocator().isLocationServiceEnabled();
   }
 
+  static Future<GeolocationStatus> checkPermission() async {
+    GeolocationStatus geolocationStatus = await Geolocator()
+        .checkGeolocationPermissionStatus();
+    return geolocationStatus;
+  }
+
   static Future<Position> getOneTimeLocation() async {
 
-    GeolocationStatus geolocationStatus = await Geolocator()
-        .checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.locationWhenInUse);
+    //GeolocationStatus geolocationStatus = await Geolocator()
+        //.checkGeolocationPermissionStatus(locationPermission: GeolocationPermission.location);
+
+    GeolocationStatus geolocationStatus = await GPSLocator.checkPermission();
 
 
     switch (geolocationStatus) {
       case GeolocationStatus.granted:
         {
-          print("#1");
+
           return Geolocator().getCurrentPosition(
               desiredAccuracy: LocationAccuracy.high)
               .timeout(Duration(seconds: _gpsCaptureTimeOut))
               .then((_position) {
                 print("Getting location data....");
-            //getting position without problems
-            return _position;
+              //getting position without problems
+              return _position;
           }).catchError((error) {
             return null;
           });
         }
-      case GeolocationStatus.denied:{
-        print("Location denied");
-      }
+      /*case GeolocationStatus.denied:{
+
+           return _askPermission();
+
+      }*/
     }
     return null;
   }
+
+
+  static askPermission() async {
+    //PermissionHandler().requestPermissions([PermissionGroup.locationWhenInUse]).then(GPSLocator._onStatusRequest);
+    await PermissionHandler().requestPermissions([PermissionGroup.locationWhenInUse]);
+  }
+
+  static _onStatusRequest(Map<PermissionGroup,PermissionStatus> statuses){
+    final status = statuses[PermissionGroup.locationWhenInUse];
+
+    if(status != PermissionStatus.granted){
+      PermissionHandler().openAppSettings();
+    }
+  }
+
 }
